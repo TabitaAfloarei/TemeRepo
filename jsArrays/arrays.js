7dd5fb035a6564ccd3a95dd853f09c1151d85c66
TabitaAfloarei <tabita.afloarei@gmail.com>
2024-06-01 22:47:09 +0300

    first 2 exercises in js Array homework

---
+++ jsArrays/arrays.js
@@ -0,0 +1,14 @@
+const initialValues = [false, 55, 100, undefined, ["html", 3, "css"], null, {nume: "Pam", prenume: "Beesly"}, 10, 20, true, "Dunder Mifflin"];
+
+//pct 1
+const otherValues = [false, 55, 100, undefined, ["html", 3, "css"], null,"Scranton", 150, "Michael Scott", {nume: "Pam", prenume: "Beesly"}, 10, 20, true, "Dunder Mifflin", 200];
+console.log("Other values:\n", otherValues);
+
+//pct 2
+initialValues.push(...otherValues);
+console.log("Initial values array(modified):\n", initialValues);
+
+//pct 3
+//pct 4
+//pct 5
+//pct 6
