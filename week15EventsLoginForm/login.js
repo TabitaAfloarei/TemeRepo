9eb0cad175a345b0d6e76d98c81bb8fe8ace9839
TabitaAfloarei <tabita.afloarei@gmail.com>
2024-07-11 17:08:39 +0300

    completed with event handling

--- week15EventsLoginForm/login.js
+++ week15EventsLoginForm/login.js
@@ -7,38 +7,58 @@ const passwordFeedback = document.getElementById('password-feedback');
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 form.addEventListener('submit', (e) =>{
+    e.preventDefault();//previn the default behavior al submit-ului, pt a ma ocupa de password
+    let isValid = true;
+
     //email validation
     if(!email.value.match(emailRegex)){
         e.preventDefault(); //butonul de sign in sa nu functioneze daca campul nu e valid
+        emailFeedback.innerHTML = 'Invalid email address';
+        emailFeedback.style.color = '#BE3144';
+        isValid = false;
+    } else{
+        emailFeedback.innerHTML = 'Email is validated';
+        emailFeedback.style.color = '#519872';
     }
 
     //password validation
     if(password.value.length <= 8 || password.value.length >= 20){
         e.preventDefault(); //butonul de sign in sa nu functioneze daca campul nu e valid
+        passwordFeedback.innerHTML = 'Password must be between 8 and 20 characters';
+        passwordFeedback.style.color = '#BE3144';
+        isValid = false;
+    } else{
+        passwordFeedback.innerHTML = 'Password is validated';
+        passwordFeedback.style.color = '#519872';
+    }
+
+    if(isValid){
+        emailFeedback.innerHTML = '';
+        passwordFeedback.innerHTML = '';
+
+        //homepage
+        window.location.href = './homepage.html';
     }
 
 });
 
-email.addEventListener('input', (e) =>{
+email.addEventListener('input', () =>{
     if(!email.value.match(emailRegex)){
-        emailFeedback.innerHTML = 'Email is required';
-    }else{
+        emailFeedback.innerHTML = 'Invalid email address';
+        emailFeedback.style.color = '#BE3144';
+    } else{
         emailFeedback.innerHTML = 'Email is validated';
+        emailFeedback.style.color = '#519872';
     }
 
 });
 
-password.addEventListener('input', (e) =>{
-    if(password.value.length <= 8){
-        passwordFeedback.innerHTML = 'Password must contain at least 8 characters';
+password.addEventListener('input', () =>{
+    if(password.value.length <= 8 || password.value.length > 20){
+        passwordFeedback.innerHTML = 'Password must be between 8 and 20 characters';
+        passwordFeedback.style.color = '#BE3144';
     } else{
         passwordFeedback.innerHTML = 'Password is validated';
+        passwordFeedback.style.color = '#519872';
     }
-
-    if(password.value.length >= 20){
-        passwordFeedback.innerHTML = 'Password must contain less than 20 characters';
-    } else{
-        passwordFeedback.innerHTML = 'Password is validated';
-    }
-
-})
+});
